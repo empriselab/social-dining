c111b1b5297144bee41f61d9768a2968ba4bccc0
Jan (Janko) Ondras <jo951030@gmail.com>
2022-03-14 22:37:13 -0400

    Add example of ELAN annotation extraction

---
+++ elan_annotation_extraction.py
@@ -0,0 +1,41 @@
+#######################################################################################################################
+# Example of extraction of annotations from ELAN annotation files
+#######################################################################################################################
+
+import glob
+import pympi
+
+
+# ELAN annotation tier names
+eaf_tier_names = [
+	'mouth_open',
+
+	'food_to_mouth',
+	'food_entered',
+	'food_lifted',
+
+	'drink_to_mouth',
+	'drink_entered',
+	'drink_lifted',
+
+	'napkin_to_mouth',
+	'napkin_entered',
+	'napkin_lifted',
+
+	'disruption'
+]
+
+annotations_folder = f'/home/janko/projects/social-dining/data/annotation/annotation_files'
+annotation_files = sorted(glob.glob(f'{annotations_folder}/*.eaf'))
+
+# For all annotation files
+for i in range(len(annotation_files)):
+
+	eaf_obj = pympi.Elan.Eaf(annotation_files[i])
+
+	for tier_name in eaf_tier_names:
+		assert tier_name in eaf_obj.get_tier_names(), f"WARNING: no '{tier_name}' annotations found for: {annotation_files[i]}"
+
+		# times are in milliseconds
+		for (start_time, end_time, value) in eaf_obj.get_annotation_data_for_tier(tier_name):
+			print(start_time, end_time, value)
